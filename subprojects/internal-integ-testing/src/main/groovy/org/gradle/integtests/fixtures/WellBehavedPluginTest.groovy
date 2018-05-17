2d45356985c9821f68d28953107edb1ba9ee5e34
Sterling Greene <sterling@gradle.com>
2018-05-17 00:28:10 -0400

    Make Antlr lazy

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/WellBehavedPluginTest.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/WellBehavedPluginTest.groovy
@@ -92,7 +92,6 @@ abstract class WellBehavedPluginTest extends AbstractIntegrationSpec {
             'scala',
             'java-gradle-plugin',
 
-            'antlr',
             'maven-publish',
             'ivy-publish',
             'ear',
